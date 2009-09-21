dc76928e6433b21b898a2fc30bc258fed608e47f
Alex Oberhauser <oberhauseralex@networld.to>
2009-09-21 20:00:53 +0200

    Implementing \!quote and \!uptime.

--- ircbot/ircbot.py
+++ ircbot/ircbot.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this software.  If not, see <http://www.gnu.org/licenses/>
 import sys, os, signal, socket
-import string
+import string, random
 import ConfigParser
 import datetime, time
 
@@ -77,9 +77,53 @@ class IRCBot:
         if ( privmsg == 'uptime' ):
             msg = 'Master Yoda long time here is. '
             msg += self.getUptime()
+        elif ( privmsg == 'quote' ):
+            msg = self.getQuote()
         else:
             msg = 'Not if anything to say about it!'
         self.socket.send('PRIVMSG ' + self.getUser(line[0]) + ' :' + msg + '\r\n')
+
+    def reactOnMSG(self, line):
+        privmsg = line[3].strip(':')
+        if ( privmsg == '!uptime' ):
+            msg = 'Master Yoda long time here is. '
+            msg += self.getUptime()
+        elif ( privmsg == '!quote' ):
+            msg = self.getQuote()
+        else:
+            return
+        self.socket.send('PRIVMSG ' + self.CHANNEL + ' :' + msg + '\r\n')
+
+    def getQuote(self):
+        quotes = [ ]
+        quotes.append("Agree with you, the council does. Your apprentice, Skywalker will be.")
+        quotes.append("Always two there are, no more, no less: a master and an apprentice.")
+        quotes.append("Fear is the path to the Dark Side. Fear leads to anger, anger leads to hate; hate leads to suffering. I sense much fear in you." )
+        quotes.append("Qui-Gon's defiance I sense in you.")
+        quotes.append("Truly wonderful the mind of a child is.")
+        quotes.append("Around the survivors a perimeter create.")
+        quotes.append("Lost a planet Master Obi-Wan has. How embarrassing ... how embarrassing.")
+        quotes.append("Victory, you say? Master Obi-Wan, not victory. The shroud of the Dark Side has fallen. Begun the Clone War has.")
+        quotes.append("Much to learn you still have ... my old padawan. ... This is just the beginning!")
+        quotes.append("Twisted by the Dark Side young Skywalker has become.")
+        quotes.append("The boy you trained, gone he is, consumed by Darth Vader.")
+        quotes.append("Death is a natural part of life. Rejoice for those around you who transform into the Force. Mourn them do not. Miss them do not. Attachment leads to jealousy. The shadow of greed that is. Train yourself to let go of everything you fear to lose.")
+        quotes.append("The fear of loss is a path to the Dark Side.")
+        quotes.append("If into the security recordings you go, only pain will you find.")
+        quotes.append("Not if anything to say about it I have")
+        quotes.append("Great warrior, hmm? Wars not make one great.")
+        quotes.append("Do or do not; there is no try.")
+        quotes.append("Size matters not. Look at me. Judge me by my size, do you?")
+        quotes.append("That is why you fail.")
+        quotes.append("No! No different. Only different in your mind. You must unlearn what you have learned.")
+        quotes.append("Always in motion the future is.")
+        quotes.append("Reckless he is. Matters are worse.")
+        quotes.append("No. There is another. ...")
+        quotes.append("When nine hundred years old you reach, look as good, you will not.")
+        quotes.append("There is ... another ... Sky ... walker. ...")
+        quotes.append("When 900 years old you reach, look as good you will not ehh.")
+        rand = random.randint(0, 3)
+        return quotes[rand]
 
     def greeting(self, line):
         msg = 'Hi ' + self.getUser(line[0]) + ' my friend. May the force be with you.'
@@ -128,6 +172,8 @@ class IRCBot:
                 try:
                     if ( self.getUser(line[0]) != self.NICKNAME and line[1] == 'PRIVMSG' and line[2] == self.NICKNAME ):
                         self.reactOnPRIVMSG(line)
+                    elif ( self.getUser(line[0]) != self.NICKNAME and line[1] == 'PRIVMSG' and line[2] == self.CHANNEL):
+                        self.reactOnMSG(line)
                     elif ( self.getUser(line[0]) != self.NICKNAME and line[1] == 'JOIN' ):
                         self.greeting(line)
                 except Exception:
