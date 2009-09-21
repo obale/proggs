ab9d8dbe1242e81f65c102959b91955371103e78
Alex Oberhauser <oberhauseralex@networld.to>
2009-09-21 21:49:48 +0200

    create_quotes.sql and quotes.db

---
+++ ircbot/create_quotes.sql
@@ -0,0 +1,34 @@
+/**
+ * sqlite3 quotes.db < create_quotes.sql
+ */
+DROP TABLE yoda_en;
+CREATE TABLE yoda_en (id integer, quote VARCHAR);
+
+INSERT INTO yoda_en (id, quote) VALUES(1, "Agree with you, the council does. Your apprentice, Skywalker will be.");
+INSERT INTO yoda_en (id, quote) VALUES(2, "Always two there are, no more, no less: a master and an apprentice.");
+INSERT INTO yoda_en (id, quote) VALUES(3, "Fear is the path to the Dark Side. Fear leads to anger, anger leads to hate; hate leads to suffering. I sense much fear in you.");
+INSERT INTO yoda_en (id, quote) VALUES(4, "Qui-Gon's defiance I sense in you.");
+INSERT INTO yoda_en (id, quote) VALUES(5, "Truly wonderful the mind of a child is.");
+INSERT INTO yoda_en (id, quote) VALUES(6, "Around the survivors a perimeter create.");
+INSERT INTO yoda_en (id, quote) VALUES(7, "Lost a planet Master Obi-Wan has. How embarrassing ... how embarrassing.");
+INSERT INTO yoda_en (id, quote) VALUES(8, "Victory, you say? Master Obi-Wan, not victory. The shroud of the Dark Side has fallen. Begun the Clone War has.");
+INSERT INTO yoda_en (id, quote) VALUES(9, "Much to learn you still have ... my old padawan. ... This is just the beginning!");
+INSERT INTO yoda_en (id, quote) VALUES(10, "Twisted by the Dark Side young Skywalker has become.");
+INSERT INTO yoda_en (id, quote) VALUES(11, "The boy you trained, gone he is, consumed by Darth Vader.");
+INSERT INTO yoda_en (id, quote) VALUES(12, "Death is a natural part of life. Rejoice for those around you who transform into the Force. Mourn them do not. Miss them do not. Attachment leads to jealousy. The shadow of greed that is . Train yourself to let go of everything you fear to lose.");
+INSERT INTO yoda_en (id, quote) VALUES(13, "The fear of loss is a path to the Dark Side.");
+INSERT INTO yoda_en (id, quote) VALUES(14, "If into the security recordings you go, only pain will you find.");
+INSERT INTO yoda_en (id, quote) VALUES(15, "Not if anything to say about it I have.");
+INSERT INTO yoda_en (id, quote) VALUES(16, "Great warrior, hmm? Wars not make one great.");
+INSERT INTO yoda_en (id, quote) VALUES(17, "Do or do not; there is no try.");
+INSERT INTO yoda_en (id, quote) VALUES(18, "Size matters not. Look at me. Judge me by my size, do you?");
+INSERT INTO yoda_en (id, quote) VALUES(19, "That is why you fail.");
+INSERT INTO yoda_en (id, quote) VALUES(20, "No! No different. Only different in your mind. You must unlearn what you have learned.");
+INSERT INTO yoda_en (id, quote) VALUES(21, "Always in motion the future is.");
+INSERT INTO yoda_en (id, quote) VALUES(22, "Reckless he is. Matters are worse.");
+INSERT INTO yoda_en (id, quote) VALUES(23, "No. There is another. ...");
+INSERT INTO yoda_en (id, quote) VALUES(24, "When nine hundred years old you reach, look as good, you will not.");
+INSERT INTO yoda_en (id, quote) VALUES(25, "There is ... another ... Sky ... walker. ...");
+INSERT INTO yoda_en (id, quote) VALUES(26, "When 900 years old you reach, look as good you will not ehh.");
+
+SELECT * FROM yoda_en;
