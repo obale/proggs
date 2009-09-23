a8273c6e387ccc8f1ec1b97890bf535a0da32ef1
Alex Oberhauser <oberhauseralex@networld.to>
2009-09-23 13:27:23 +0200

    Adding UTF-8 support to the private message system and making the names (from/to) case insensitive.

--- ircbot/install/create_gettext.sh
+++ ircbot/install/create_gettext.sh
@@ -5,9 +5,9 @@
 #msginit --input=lang/ircbot.pot --locale=lang/en_EN
 #msginit --input=lang/ircbot.pot --locale=lang/de_DE
 
-#rm -rf lang/en_EN lang/de_DE
-#mkdir -p lang/en_EN/LC_MESSAGES
-#mkdir -p lang/de_DE/LC_MESSAGES
+rm -rf lang/en_EN
+mkdir -p lang/en_EN/LC_MESSAGES
+mkdir -p lang/de_DE/LC_MESSAGES
 
 msgfmt --output-file=lang/en_EN/LC_MESSAGES/ircbot.mo lang/en_EN.po
 msgfmt --output-file=lang/de_DE/LC_MESSAGES/ircbot.mo lang/de_DE.po
--- ircbot/modules/privateMessage.py
+++ ircbot/modules/privateMessage.py
@@ -13,6 +13,7 @@ def init():
     dt = datetime.datetime.now()
     init.NOW = dt.strftime("%A, %d. %B %Y %H:%M")
     init.conn = sqlite3.connect(init.DBFILE)
+    init.conn.text_factory = str
     init.curs = init.conn.cursor()
 
 def clean():
@@ -20,22 +21,30 @@ def clean():
     init.curs.close()
 
 def sendMessage(soc, fromuser, touser, line):
-    _ = lang.getGettext
+    fromuser = string.lower(fromuser)
+    touser = string.lower(touser)
+    _ = lang.getGettext()
     message = ""
     for msg in line:
         if msg == line[0] or msg == line[1] or msg == line[2] or msg == line[3] or msg == line[4]: continue
         msg = string.strip(msg, '["\']')
         message += msg + " "
+    if message == "":
+        return
     init()
     query = "INSERT INTO " + init.DBNAME + " ( fromuser, touser, time, message )"
     query += "VALUES ( '" + fromuser + "', '" + touser + "', '" + init.NOW + "', '" + message + "')"
     try:
         init.curs.execute(query)
     except Exception:
-        soc.send('PRIVMSG ' + fromuser + ' :Error!\r\n')
+        errmsg = _('Error there is occurred!')
+        soc.send('PRIVMSG ' + fromuser + ' :'+ errmsg + '\r\n')
+    msg = _('Sent!')
+    soc.send('PRIVMSG ' + fromuser + ' :'+ msg + '\r\n')
     clean()
 
 def receiveMessages(soc, touser):
+    touser = string.lower(touser)
     init()
     query = "SELECT fromuser, time, message FROM " + init.DBNAME + " where touser='" + touser + "'"
     init.curs.execute(query)
