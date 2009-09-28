5423b071570008df48dbb3cb279a9c365b89dcb9
Alex Oberhauser <oberhauseralex@networld.to>
2009-09-28 16:41:04 +0200

    Making a new repository for ircbot and tweetC2

--- tweetC2/db/create.sql
+++
@@ -1,6 +0,0 @@
-/**
- * sqlite3 tweetC2.db < create.sql
- */
-DROP TABLE handled_commands;
-
-CREATE TABLE handled_commands ( id INTEGER PRIMARY KEY, username VARCHAR,  message VARCHAR, created_at VARCHAR);
